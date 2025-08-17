272a5bef669e145f8e7a11c01c87e277a6418972
Banks Nussman <banks@nussman.us>
2025-08-17 17:26:02 -0400

    add extra context

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import createClient from "openapi-fetch";
 import type { paths as OSRM } from "./osrm";
 
 export const osrm = createClient<OSRM>({
-  baseUrl: "https://osrm.ridebeep.app/",
+  baseUrl: "https://osrm.ridebeep.app",
+  // baseUrl: "https://router.project-osrm.org",
   // baseUrl: "https://routing.openstreetmap.de/routed-car",
 });
 export type { OSRM };
